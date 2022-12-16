f1a7bd3b5303853741574433254b66f84422774e
starwook <star.wookk@gmail.com>
2022-12-16 21:25:01 +0900

    refactor: refactor main method

--- src/main/java/bridge/Application.java
+++ src/main/java/bridge/Application.java
@@ -3,6 +3,8 @@ package bridge;
 public class Application {
 
     public static void main(String[] args) {
+        BridgeMachine bridgeMachine = new BridgeMachine();
+        bridgeMachine.init();
         // TODO: 프로그램 구현
     }
 }
